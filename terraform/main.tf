6dde4bcdf29dca103f054f2c8a29a54436a8ff83
Ray Sinnema <3496035+RaySinnema@users.noreply.github.com>
2025-03-16 14:55:00 -0600

    Run tf init before appy (#10)

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,10 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
   backend "s3" {
     bucket = "dev-connect-terraform-state"
     key    = "terraform.tfstate"
