538efb707f38b61179b9cecdfa556f144f292ffb
Ray Sinnema <rssinnema@yahoo.com>
2025-03-18 19:36:26 -0600

    Fix variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,17 +12,17 @@ variable "project" {
 variable "frontend_bucket" {
   type        = string
   description = "The S3 bucket to deploy the frontend distribution into"
-  default     = "${var.project}-frontend"
+  default     = "devconnect-hunter-frontend"
 }
 
 variable "cloudfront_origin" {
   type        = string
   description = "The origin ID of the CloudFront distribution"
-  default     = "${var.project}-cloudfront"
+  default     = "devconnect-hunter-cloudfront"
 }
 
 variable "domain" {
   type        = string
   description = "The domain to bind to CloudFront"
-  default     = "${var.project}.org"
+  default     = "devconnect-hunter.org"
 }
