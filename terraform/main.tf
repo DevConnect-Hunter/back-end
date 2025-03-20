a18413deac3589eef9c0fb9850b5129cc01fad66
Ray Sinnema <rssinnema@yahoo.com>
2025-03-19 19:20:00 -0600

    Use the certificate in CloudFront

--- terraform/main.tf
+++ terraform/main.tf
@@ -63,7 +63,9 @@ resource "aws_cloudfront_distribution" "frontend_cloudfront" {
     }
   }
   viewer_certificate {
-    cloudfront_default_certificate = true
+    acm_certificate_arn = aws_acm_certificate.cert.arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
   }
 }
 
