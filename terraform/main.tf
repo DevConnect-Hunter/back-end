ea57f48d8fd868ef68d99b9a5507c4ab3fa6fd1d
Ray Sinnema <rssinnema@yahoo.com>
2025-03-23 09:23:56 -0600

    Attempt to prevent unnecessary updates to bucket policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -84,14 +84,13 @@ resource "aws_cloudfront_origin_access_identity" "oai_frontend" {
 # Give CloudFront access to the S3 bucket
 resource "aws_s3_bucket_policy" "frontend_bucket_access_policy" {
   bucket = aws_s3_bucket.frontend_bucket.id
-
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
         Effect = "Allow"
         Principal = {
-          AWS = "arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity ${aws_cloudfront_origin_access_identity.oai_frontend.iam_arn}"
+          AWS = aws_cloudfront_origin_access_identity.oai_frontend.iam_arn
         }
         Action   = "s3:GetObject"
         Resource = "${aws_s3_bucket.frontend_bucket.arn}/*"
